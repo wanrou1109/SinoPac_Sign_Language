add8efb3a9f6d0cbb19cb2f103028531ef28d23f
wanroulin <118650467+wanrou1109@users.noreply.github.com>
2025-03-27 01:15:29 +0800

    conservation頁面

--- 永豐產學/App/src/components/AuthScreen.js
+++ 永豐產學/App/src/components/AuthScreen.js
@@ -19,16 +19,16 @@ const AuthScreen = () => {
 
             <select className='branch-dropdown' value={selectedBranch} onChange={(e) => setSelectedBranch(e.target.value)}>
                 <option value="">請選擇分行</option>
-                <option value="113 台北士林分行">113 台北士林分行</option>
-                <option value="135 台北社子分行">135 台北社子分行</option>
-                <option value="157 台北雅蘭分行">157 台北雅蘭分行</option>
-                <option value="131 台北士東分行">131 台北士東分行</option>
-                <option value="034 台北天母分行">034 台北天母分行</option>
-                <option value="101 台北忠孝東路分行">101 台北忠孝東路分行</option>
-                <option value="005 台北忠孝分行">005 台北忠孝分行</option>
-                <option value="001 台北世貿分行">001 台北世貿分行</option>
-                <option value="160 台北仁愛分行">160 台北仁愛分行</option>
-                <option value="136 台北信義分行">136 台北信義分行</option>
+                <option value="113 士林分行">113 台北士林分行</option>
+                <option value="135 社子分行">135 台北社子分行</option>
+                <option value="157 雅蘭分行">157 台北雅蘭分行</option>
+                <option value="131 士東分行">131 台北士東分行</option>
+                <option value="034 天母分行">034 台北天母分行</option>
+                <option value="101 忠孝東路分行">101 台北忠孝東路分行</option>
+                <option value="005 忠孝分行">005 台北忠孝分行</option>
+                <option value="001 世貿分行">001 台北世貿分行</option>
+                <option value="160 仁愛分行">160 台北仁愛分行</option>
+                <option value="136 信義分行">136 台北信義分行</option>
             </select>
 
             <button 
